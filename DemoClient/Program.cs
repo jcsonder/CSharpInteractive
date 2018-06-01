0477536d85876bb635f9c87b9c39475c04eb4999
Sonderegger, Jean-Claude <jeanclaude.sonderegger@metrohm.com>
2018-06-01 07:02:21 +0200

    Solution with 2 project: Console client and library with Documentation folder

---
+++ DemoClient/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoClient
+{
+    using System;
+    using DemoLibrary;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var energyDemandRaw = EnergyData.GetCountries().Result;
+            Console.WriteLine("Raw Data:");
+            Console.WriteLine(energyDemandRaw.Substring(1, 1000) + "...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DemoLibrary/EnergyData.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class EnergyData
+    {
+        public static async Task<string> GetCountries()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync("http://services.groupkt.com/country/get/all");
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
